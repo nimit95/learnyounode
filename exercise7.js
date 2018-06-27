493519009af2d18f6da3186a9a94c09237ff016b
Nimit <nimitagg95@gmail.com>
2018-06-27 14:35:13 +0530

    file name fix
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- exercise7.js
+++ exercise7.js
@@ -7,15 +7,12 @@ http.get(process.argv[2], res => {
   //console.log(res);
   res.setEncoding('utf8');
 
-  let data = '';
+
 
   res.on('data', chunk => {
-    data += chunk;
+    console.log(chunk);
   })
 
-  res.on('end', ()=> {
-    console.log(data.split("").length);
-    console.log(data);
-  });
+
 
 });
